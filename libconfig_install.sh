546579e779acc40b001f40253fb5cd26ac59b4b5
AlexanderTutyhin <sasha.t@twigames.co>
2025-06-26 14:08:57 +0300

    libconfig installer fix

--- libconfig_install.sh
+++ libconfig_install.sh
@@ -2,10 +2,15 @@
 #This script will install libconfig library
 
 # create folder for libconfig library
-mkdir -p ~/libs/libconfig
-cd ~/libs/libconfig
+mkdir -p ~/libs
+cd ~/libs
+
+sudo rm -rf libconfig
+mkdir -p libconfig
+cd libconfig
 
 git clone https://github.com/hyperrealm/libconfig.git .
+git reset e7e8dee409632f9be89febed88a9707159e20e75 --hard
 
 #build & install library
 mkdir build 
